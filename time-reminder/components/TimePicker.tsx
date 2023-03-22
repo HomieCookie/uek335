5de9cbb20ea246bb5248b536900f886c299d16c2
Céline Sinzig <celine.sinzig@gmx.ch>
2023-03-22 13:40:05 +0100

    added TSDoc

--- time-reminder/components/TimePicker.tsx
+++ time-reminder/components/TimePicker.tsx
@@ -3,35 +3,40 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { TimePickerModal } from "react-native-paper-dates";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import StorageService from "../services/StorageService";
-
+/**
+ * It is a timepicker component that allows you to select a specific time
+ */
 export default function TimePicker() {
   const [visible, setVisible] = React.useState(false);
   const [hours, setHours] = React.useState(1);
   const [minutes, setMinutes] = React.useState(0);
 
   useEffect(() => {
-    StorageService.get('hour').then((hour) => {
+    StorageService.get("hour").then((hour) => {
       if (hour) {
         setHours(Number(hour));
       }
-    })
-    StorageService.get('minute').then((minute) => {
+    });
+    StorageService.get("minute").then((minute) => {
       if (minute) {
         setMinutes(Number(minute));
       }
-    })
-  }, [])
+    });
+  }, []);
 
   useEffect(() => {
-    StorageService.set('hour', hours.toString());
-    StorageService.set('minute', minutes.toString());
-  }, [hours, minutes])
+    StorageService.set("hour", hours.toString());
+    StorageService.set("minute", minutes.toString());
+  }, [hours, minutes]);
 
   function onDismiss() {
     setVisible(false);
   }
 
   const onConfirm = React.useCallback(
+    /**
+     * This function is triggered when the selected values are confirmed and will set the selected values
+     */
     (params: any) => {
       setVisible(false);
       setHours(params.hours);
@@ -40,11 +45,14 @@ export default function TimePicker() {
     [setVisible]
   );
 
-  const selectedTime = `${hours.toLocaleString('de-CH', {
+  const selectedTime = `${hours.toLocaleString("de-CH", {
+    /**
+     * @returns The time is gonna turn into a String with 2 digits for hours and minutes
+     * */
     minimumIntegerDigits: 2,
-  })}:${minutes.toLocaleString('de-CH', {
+  })}:${minutes.toLocaleString("de-CH", {
     minimumIntegerDigits: 2,
-  })}`
+  })}`;
 
   return (
     <SafeAreaProvider style={{ margin: 10 }}>
@@ -61,7 +69,7 @@ export default function TimePicker() {
           use24HourClock={true}
         />
       </View>
-    </SafeAreaProvider >
+    </SafeAreaProvider>
   );
 }
 
@@ -75,5 +83,5 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 35,
-  }
+  },
 });
